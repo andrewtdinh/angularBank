ce8eccb81217b0cebeba1493e0889699f0f2787f
Andrew Dinh <andrewdinh74@yahoo.com>
2015-04-27 14:01:01 -0700

    Finish app

--- client/models/account.js
+++ client/models/account.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('angBank')
-.factory('Account', function($rootScope, $firebaseObject){
+.factory('Account', function($rootScope, $firebaseObject, $firebaseArray, $window){
   var fbUser;
   var afUser;
 
@@ -11,15 +11,34 @@ angular.module('angBank')
   Account.init = function(){
     fbUser = $rootScope.fbRoot.child('users/' + $rootScope.activeUser.uid);
     afUser = $firebaseObject(fbUser);
+    return afUser;
+  };
+
+  Account.delTransaction = function(tx, index){
+    var fbTransactions = fbUser.child('accounts/' + tx.type);
+    var afTransactions = $firebaseArray(fbTransactions);
+    afTransactions.$loaded().then(function(){
+      var foundTx = afTransactions[index];
+      afTransactions.$remove(foundTx);
+    });
+  };
+
+  Account.addTransaction = function(name, tx){
+    var transaction = angular.copy(tx);
+    transaction.date = transaction.date.getTime();
+    transaction.createdAt = $window.Firebase.ServerValue.TIMESTAMP;
+    transaction.name = name;
+    var fbTransactions = fbUser.child('accounts/' + tx.type);
+    var afTransactions = $firebaseArray(fbTransactions);
+    afTransactions.$add(transaction);
   };
 
   Account.add = function(name){
-    console.log('Before change: ', afUser.names);
-    var names = afUser.names ? afUser.names.split(','): [];
+    var names = afUser.names ? afUser.names.split(',') : [];
     names.push(name);
+
     afUser.names = names.join(',');
-    afUser.$save();
-    // $scope.accountName = 
+    return afUser.$save();
   };
 
   return Account;
--- client/views/accounts/accounts.js
+++ client/views/accounts/accounts.js
@@ -2,9 +2,24 @@
 
 angular.module('angBank')
 .controller('AccountsCtrl', function($scope, Account){
-  Account.init();
+  var afUser = $scope.afUser = Account.init();
+  afUser.$loaded().then(syncNames);
 
   $scope.addAccount = function(name){
-    Account.add(name)
+    Account.add(name).then(syncNames);
+    $scope.accountName = '';
   };
+
+  $scope.delTransaction = function(tx, index){
+    Account.delTransaction(tx, index);
+  };
+
+  $scope.addTransaction = function(name, tx){
+    Account.addTransaction(name, tx);
+    $scope.tx = {};
+  };
+
+  function syncNames(){
+    $scope.names = afUser.names ? afUser.names.split(',') : [];
+  }
 });
